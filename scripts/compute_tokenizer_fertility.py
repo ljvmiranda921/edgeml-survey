8c785501b369adcce225d7e0573f89fa8e1ff55c
Lester James Miranda <ljvmiranda@gmail.com>
2026-02-13 11:15:15 +0000

    [wip] Update

--- scripts/compute_tokenizer_fertility.py
+++ scripts/compute_tokenizer_fertility.py
@@ -1,58 +1,64 @@
 import argparse
-import logging
-import sys
 import regex
 from pathlib import Path
 from transformers import AutoTokenizer
 from tqdm import tqdm
 
-from datasets import load_dataset
-
-logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
-    datefmt="%Y-%m-%d %H:%M:%S",
-    handlers=[logging.StreamHandler(sys.stdout)],
-    level=logging.INFO,
-)
+from datasets import get_dataset_config_names, load_dataset
 
 
 def get_args():
     # fmt: off
-    parser = argparse.ArgumentParser(description="Compute tokenizer fertility.")
-    parser.add_argument("--input_dataset", type=str, required=True, help="Path to the dataset to compute tokenizer fertility.")
+    parser = argparse.ArgumentParser(description="Compute tokenizer fertility from the Flores-200 dataset.")
+    parser.add_argument("--input_dataset", type=str, default="openlanguagedata/flores_plus", help="Path to the dataset to compute tokenizer fertility.")
     parser.add_argument("--output_path", type=Path, help="Path to save the output CSV file.")
-    parser.add_argument("--split", type=str, default="train", help="Dataset split name.")
+    parser.add_argument("--split", type=str, default="dev", help="Dataset split name.")
     parser.add_argument("--tokenizer", type=str, required=True, help="HuggingFace ID to tokenizer.")
     parser.add_argument("--text_field", type=str, default="text", help="Input field in the dataset that contains the text to tokenize.")
     # fmt: on
+    return parser.parse_args()
 
 
 def main():
     args = get_args()
-    logging.info(f"Loading dataset: {args.input_dataset} (split={args.split})")
-    dataset = load_dataset(args.input_dataset, split=args.split)
-
-    total_words = 0
-    total_bytes = 0
-    for example in dataset:
-        total_words += n_words(example[args.text_field])
-        total_bytes += n_bytes(example[args.text_field])
+    print(f"Loading dataset: {args.input_dataset} (split={args.split})")
+    subsets = [s for s in get_dataset_config_names(args.input_dataset) if s != "default"]  # fmt: skip
 
     tokenizer = AutoTokenizer.from_pretrained(args.tokenizer)
 
     def tokenize_fn(eg):
         return tokenizer(eg[args.text_field])
 
-    logging.debug("Performing tokenization...")
-    process_ds = dataset.map(tokenize_fn, batched=True)
-    total_tokens = 0
-    for example in tqdm(process_ds):
-        example_tokens = len(example["input_ids"])
-        total_tokens += example_tokens
+    outputs = {}
+    for subset in subsets:
+        dataset = load_dataset(args.input_dataset, subset, split=args.split)
 
-    tokens_per_word = total_tokens / total_words
-    tokens_per_byte = total_tokens / total_bytes
-    breakpoint()
+        total_words = 0
+        total_bytes = 0
+        for example in dataset:
+            total_words += n_words(example[args.text_field])
+            total_bytes += n_bytes(example[args.text_field])
+
+        process_ds = dataset.map(tokenize_fn, batched=True)
+        total_tokens = 0
+        for example in tqdm(process_ds):
+            example_tokens = len(example["input_ids"])
+            total_tokens += example_tokens
+
+        tokens_per_word = total_tokens / total_words
+        tokens_per_byte = total_tokens / total_bytes
+
+        results = {
+            "tpw": tokens_per_word,
+            "tpb": tokens_per_byte,
+            "tt": total_tokens,
+            "tw": total_words,
+            "tb": total_bytes,
+        }
+
+        outputs[subset] = results
+
+        tqdm.write(results)
 
 
 def n_words(sentence: str) -> int:
